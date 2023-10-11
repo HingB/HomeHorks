5871a8578d09f0e3c05e8482b38ec53bb6dfba88
HingB <50633061+HingB@users.noreply.github.com>
2023-10-11 22:21:45 +0300

    Add files via upload

---
+++ FirstNotSeven.cs
@@ -0,0 +1,24 @@
+﻿namespace Homework
+{
+    public static class Mission2
+    {
+        public static void Main()
+        {
+            double a, b, c;
+            double square, perimeter;
+
+            Console.WriteLine("Введите a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+
+            square = (a * b) / 2;
+            perimeter = a + b + c;
+
+            Console.WriteLine("Площадь - " + square);
+            Console.WriteLine("Периметр  - " + perimeter);
+        }
+    }
+}
