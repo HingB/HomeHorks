8343fd068c1be8c1f785dd48efd0bbbcca03d645
HingB <50633061+HingB@users.noreply.github.com>
2023-10-11 22:20:36 +0300

    Add files via upload

---
+++ First.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Homework
+{
+    public static class Mission1
+    {
+        public static void Main()
+        {
+            float x, y;
+
+            Console.WriteLine("Введите 1 число: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 2 число: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Сумма - " + (x + y));
+            Console.WriteLine("Разность - " + (x - y));
+            Console.WriteLine("Произведение - " + (x * y));
+            Console.WriteLine("Частное - " + (x / y));
+        }
+    }
+}
